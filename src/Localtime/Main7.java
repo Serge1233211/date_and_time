ec2ba141e9fd860d4cc7eb0e4cccca3795b77336
Serge1233211 <141172587+Serge1233211@users.noreply.github.com>
2024-04-22 17:31:02 -0400

    new main

---
+++ src/Localtime/Main7.java
@@ -0,0 +1,16 @@
+package Localtime;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class Main7 {
+    public static void main(String[] args) {
+        LocalDate birthday = LocalDate.of(2019, 6, 26);
+        LocalDate nextBirthday = birthday.plusYears(1);
+        LocalDate firstBirthday = birthday.minusYears(11);
+        System.out.println(birthday);
+        System.out.println(nextBirthday);
+        System.out.println(firstBirthday);
+
+    }
+}
